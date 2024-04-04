8962a4aa68cdeda94466a9cefd530a323e5bc7ec
4str0 <122585929+Astro0710@users.noreply.github.com>
2024-04-04 20:02:13 +0200

    Update bcml_setup.ps1

--- bcml_setup.ps1
+++ bcml_setup.ps1
@@ -1,36 +1,3 @@
-﻿##[Ps1 To Exe]
-##
-##Kd3HDZOFADWE8uO1
-##Nc3NCtDXTlaDjofG5iZk2VzhUWYiUuSLqZKm0ICA0+XvuinleYgbTkZ2mzrANFKpUf0cacUMoNkeW05yE94C4I7DEvOsCLILkeh+bOCdmrAqGFbk55Lg0iWQzJDMUAVhWTw=
-##Kd3HFJGZHWLWoLaVvnQnhQ==
-##LM/RF4eFHHGZ7/K1
-##K8rLFtDXTiS5
-##OsHQCZGeTiiZ4NI=
-##OcrLFtDXTiW5
-##LM/BD5WYTiiZ49I=
-##McvWDJ+OTiiZ49I=
-##OMvOC56PFnzN8u+VslQ=
-##M9jHFoeYB2Hc8u+VslQ=
-##PdrWFpmIG2HcofKIo2QX
-##OMfRFJyLFzWE8uK1
-##KsfMAp/KUzWI0g==
-##OsfOAYaPHGbQvbyVvnQmqxugEiZ6Dg==
-##LNzNAIWJGmPcoKHc7Do3uAu/DDhlPovK2Q==
-##LNzNAIWJGnvYv7eVvnRVxmbCD1suetCL2Q==
-##M9zLA5mED3nfu77Q7TV64AuzAkoIQ+nWirOz0ZHc
-##NcDWAYKED3nfu77Q7TV64AuzAmooY8mksLi00ICw8+/+2w==
-##OMvRB4KDHmHQvbyVvnRE7Eb+TnFrb9CPtvauypKo/ubg+wf6ZLZ0
-##P8HPFJGEFzWE8pvR6FQ=
-##KNzDAJWHD2fS8u+Vxztk4EX6Ig==
-##P8HSHYKDCX3N8u+VzTsX
-##LNzLEpGeC3fMu77Ro2k3y0SO
-##L97HB5mLAnfMu77Ro2k3y0SO
-##P8HPCZWEGmaZ7/Lw7T54/AqO
-##L8/UAdDXTlaDjofG5iZk2VzhUWYiUuGeqr2zy5GA/enhtxrYXI4bQFpkgijwCE6vF/cKUJU=
-##Kc/BRM3KXxU=
-##
-##
-##fd6a9f26a06ea3bc99616d4851b372ba
 Invoke-WebRequest 'https://raw.githubusercontent.com/Astro0710/bcml-custom-installer/main/python_path.cmd' -OutFile C:/Windows/Temp/python_path.cmd
 Invoke-WebRequest 'https://www.myinstants.com/media/sounds/zeldas-melee.mp3' -OutFile C:/Windows/Temp/zelda-melee.mp3
 Invoke-WebRequest 'https://raw.githubusercontent.com/Astro0710/bcml-custom-installer/main/startup.vbs' -OutFile C:/Windows/Temp/startup.vbs
